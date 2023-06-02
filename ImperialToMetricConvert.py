215794e467ff7988a1a993c51b47402407cc4a45
Kacper Grabarczyk <kacperlukaszgrabarczyk@gmail.com>
2023-06-02 22:27:29 +0200

    Functions converting foots to milimeters, centimeters, meters, kilometers added

--- ImperialToMetricConvert.py
+++ ImperialToMetricConvert.py
@@ -13,3 +13,20 @@ def InchToMeter (dimension):
 def InchToKilometer (dimension):
     #convert inch to kilometer
     return dimension*0.0000254
+
+
+def FootToMilimeter (dimension):
+    #convert foot to milimeter
+    return dimension*304.8
+
+def FootToCentimeter (dimensin):
+    #convert foot to centimeter
+    return dimensin*30.48
+
+def FootToMeter (dimension):
+    #convert foot to meter
+    return dimension*0.3048
+
+def FootToKilometer (dimension):
+    #convert foot to kilometer
+    return dimension*0.0003048
