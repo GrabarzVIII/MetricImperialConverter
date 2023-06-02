1c1bbee4a57b1c98aa30925031d18560b1f8f84c
Kacper Grabarczyk <kacperlukaszgrabarczyk@gmail.com>
2023-06-02 22:03:19 +0200

    Functions converting centimeters to inchs, foots, yards, miles added

--- MetricToImperialConvert.py
+++ MetricToImperialConvert.py
@@ -12,10 +12,26 @@ def MilimeterToYard (dimension):
 
 def MilimeterToMile (dimension):
     #convert milimeter to mile
-    return dimension*0.0000006213689
+    return dimension*0.00000062137
 
 
-print(MilimeterToInch(300))
-print(MilimeterToFoot(300))
-print(MilimeterToYard(300))
-print(MilimeterToMile(300))
+def CentimeterToInch (dimension):
+    #convert centimeter to inch
+    return dimension*0.3937
+
+def CentimeterToFoot (dimension):
+    #convert centimeter to foot
+    return dimension*0.032808
+
+def CentimeterToYard (dimension):
+    #convert centimeter to yard
+    return dimension*0.010936
+
+def CentimeterToMile (dimension):
+    #convert centimeter to mile
+    return dimension*0.0000062137
+
+print(CentimeterToInch(30))
+print(CentimeterToFoot(30))
+print(CentimeterToYard(30))
+print(CentimeterToMile(30))
