ee657bc232a4c4705fe8b79c85e1b32f532e9f42
Kacper Grabarczyk <kacperlukaszgrabarczyk@gmail.com>
2023-06-02 21:55:41 +0200

    Fonction converting milimeter to inch, foot, yard, mile added

---
+++ MetricToImperialConvert.py
@@ -0,0 +1,21 @@
+def MilimeterToInch (dimension):
+    #convert milimeter to inch
+    return dimension*0.03937
+
+def MilimeterToFoot (dimension):
+    #conver milimeter to foot
+    return dimension*0.0032808
+
+def MilimeterToYard (dimension):
+    #convert milimeter to yard
+    return dimension*0.0010936
+
+def MilimeterToMile (dimension):
+    #convert milimeter to mile
+    return dimension*0.0000006213689
+
+
+print(MilimeterToInch(300))
+print(MilimeterToFoot(300))
+print(MilimeterToYard(300))
+print(MilimeterToMile(300))
